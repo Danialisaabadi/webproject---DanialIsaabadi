8dd270741e64e91f087422ee491d74564c4bdab0
Danialisaabadi <danial.isaabadi81@gmail.com>
2025-06-03 18:11:17 +0330

    Improve user handling in home method
    Improve user handling in FrontController::home method:
    - Use array_key_exists to safely access $_GET['id']
    - Check if User::find($id) returns a valid user before calling composite()
    - Show message if user does not exist
    - Exit after debug output to prevent further execution

--- app/Controller/FrontController.php
+++ app/Controller/FrontController.php
@@ -4,9 +4,15 @@ namespace App\Controller;
 use App\Model\User;
 class FrontController{
     public static function home(){
-        $id = $_GET['id'];
+        $id = array_key_exists('id',$_GET)? $_GET['id']:0;
         $user  = User::find($id);
-        var_dump($user->composite());
+        if($user)
+        {
+            var_dump($user->composite()); 
+        }else{
+            echo('User not exist');
+        }
+        exit;
         return view('home.php');
     }
     public static function about(){
